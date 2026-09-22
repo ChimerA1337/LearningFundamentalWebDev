66f17f14705474bf3e992cf6fe318cb5a3bca2f3
ChimerA1337 <endlesst1m52@gmail.com>
2026-09-22 19:31:17 +0200

    Added a digital clock

--- passwordGen.js
+++ passwordGen.js
@@ -11,6 +11,8 @@ const numbersCheck = document.getElementById("allowNumbers");
 const generatePasswordButton = document.getElementById("generatePassword");
 const passwordText = document.getElementById("password");
 const passwordLengthInput = document.getElementById("length");
+const copiedPopup = document.getElementById("popup");
+copiedPopup.textContent = 'Copied password!';
 
 let password = generatePassword(
     passwordLength, 
@@ -25,7 +27,7 @@ passwordText.innerHTML = `<i class="fa-solid fa-copy"></i> ${password} <i class=
 generatePasswordButton.onclick = function() {
     if(lowercaseCheck.checked) allowLowercase = true;
     else allowLowercase = false;
-    if(lowercaseCheck.checked) allowUppercase = true;
+    if(uppercaseCheck.checked) allowUppercase = true;
     else allowUppercase = false;
     if(symbolsCheck.checked) allowSymbols = true;
     else allowSymbols = false;
@@ -45,6 +47,14 @@ generatePasswordButton.onclick = function() {
     if(!allowLowercase && !allowUppercase && !allowSymbols && !allowNumbers)
         passwordText.textContent = "Please check atleast one box.";
     else passwordText.innerHTML = `<i class="fa-solid fa-copy"></i> ${password} <i class="fa-solid fa-copy"></i>`
+}
+
+void popup.offsetWidth;
+passwordText.onclick = function() {
+    navigator.clipboard.writeText(password);
+    copiedPopup.classList.remove('show');
+    void popup.offsetWidth;
+    copiedPopup.classList.add('show');
 }
 
 function generatePassword(  
